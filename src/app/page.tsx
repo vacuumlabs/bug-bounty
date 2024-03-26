9bc41f20403f83390ec460d09768537d65ce32cd
Matej Falat <matej.falat@gmail.com>
2024-03-26 09:00:08 +0100

    feat: add Sign up page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,13 @@ const Home = async () => {
           className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20">
           {session ? 'Sign out' : 'Sign in'}
         </Link>
+        {!session && (
+          <Link
+            href={'/api/auth/signup'}
+            className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20">
+            Sign up
+          </Link>
+        )}
       </div>
     </main>
   )
