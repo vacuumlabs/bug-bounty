da9a59c1f7fb0c4057178b58a51a120d5e9f5989
Matej Falat <matej.falat@gmail.com>
2024-04-16 19:21:02 +0200

    refactor: move ConnectWallet into a dedicated component

--- src/app/profile/connect-wallet/page.tsx
+++ src/app/profile/connect-wallet/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import {Button} from '@/components/ui/Button'
 import {requirePageSession} from '@/server/utils/auth'
 import {prefetchGetUser} from '@/lib/queries/getUser'
-import ConnectWallet from '@/components/sections/profile/ConnectWallet'
+import RegisterWalletAddress from '@/components/sections/profile/RegisterWalletAddress'
 import HydrationBoundary from '@/components/helpers/HydrationBoundary'
 
 const ConnectWalletPage = async () => {
@@ -14,7 +14,7 @@ const ConnectWalletPage = async () => {
   return (
     <main className="flex flex-col items-center gap-8 pt-[100px]">
       <HydrationBoundary>
-        <ConnectWallet />
+        <RegisterWalletAddress />
       </HydrationBoundary>
       <Button asChild variant="link">
         <Link href="/profile">Go home</Link>
