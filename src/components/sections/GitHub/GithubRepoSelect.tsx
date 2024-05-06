f5dcb356a2ac08250cd0bc02e8791161af9c2507
Ice de Vera <iestyndevera@gmail.com>
2024-05-06 18:02:52 +0800

    separate components for select files and select repo

---
+++ src/components/sections/GitHub/GithubRepoSelect.tsx
@@ -0,0 +1,79 @@
+'use client'
+
+import {ArrowLeft} from 'lucide-react'
+import {signIn, useSession} from 'next-auth/react'
+
+import {Button} from '@/components/ui/Button'
+import {useGetPublicRepos} from '@/lib/queries/github/getGitHub'
+import {GetPublicReposResponse} from '@/server/actions/github/getGithub'
+import {ScrollArea} from '@/components/ui/ScrollArea'
+
+type GithubRepoSelect = {
+  selectedRepo: GetPublicReposResponse[number] | null
+  onSelectRepo: (repo: GetPublicReposResponse[number] | null) => void
+}
+
+const GithubRepoSelect = ({selectedRepo, onSelectRepo}: GithubRepoSelect) => {
+  const session = useSession()
+
+  const {data: publicReposData, isLoading: publicReposIsLoading} =
+    useGetPublicRepos()
+
+  if (session.data?.user.provider !== 'github') {
+    return (
+      <Button
+        onClick={() => signIn('github')}
+        className="bg-gray-800 text-white">
+        Sign in with Github to select repo files
+      </Button>
+    )
+  }
+
+  return (
+    <div className="min-w-96 rounded-md border-2 border-gray-900 p-2">
+      {selectedRepo === null ? (
+        <div>
+          <span className="pb-2 font-bold">Select a repo</span>
+          <ScrollArea className="h-96 w-full" thumbClassname="bg-gray-400">
+            <div className="flex flex-col items-start gap-2">
+              <RepoList
+                repos={publicReposData}
+                isLoading={publicReposIsLoading}
+                selectRepo={(params: GetPublicReposResponse[number]) =>
+                  onSelectRepo(params)
+                }
+              />
+            </div>
+          </ScrollArea>
+        </div>
+      ) : (
+        <div>
+          <Button variant="outline" onClick={() => onSelectRepo(null)}>
+            <ArrowLeft className="h-4 w-4" />
+          </Button>
+          <span className="pl-2 font-bold">{`${selectedRepo.owner}/${selectedRepo.name}`}</span>
+        </div>
+      )}
+    </div>
+  )
+}
+
+type RepoListProps = {
+  repos: GetPublicReposResponse | undefined
+  isLoading: boolean
+  selectRepo: (params: GetPublicReposResponse[number]) => void
+}
+
+const RepoList = ({repos, isLoading, selectRepo}: RepoListProps) => {
+  if (!repos && isLoading) return <span>Loading...</span>
+
+  if (!repos || repos.length === 0) return <span>No public repos found</span>
+
+  return repos.map((repo) => (
+    <Button key={repo.id} onClick={() => selectRepo(repo)}>
+      {repo.fullName}
+    </Button>
+  ))
+}
+
+export default GithubRepoSelect
