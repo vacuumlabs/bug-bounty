8efbddf0ebb0b972e351a792e92229e811666419
Matej Falat <matej.falat@gmail.com>
2024-05-13 19:23:12 +0200

    feat: show contest counts

--- src/components/sections/contest/Contests.tsx
+++ src/components/sections/contest/Contests.tsx
@@ -7,18 +7,32 @@ import {Tabs, TabsList, TabsTrigger} from '@/components/ui/Tabs'
 import {useGetPublicContests} from '@/lib/queries/contest/getContests'
 import Skeleton from '@/components/ui/Skeleton'
 import {ContestOccurence} from '@/server/db/models'
+import {useGetPublicContestCounts} from '@/lib/queries/contest/getPublicContestCounts'
+
+const formatCount = (count: number | undefined) =>
+  count == null ? '' : ` (${count})`
 
 const Contests = () => {
   const [contestType, setContestType] = useSearchParamsContestType()
 
-  const {data, isLoading} = useGetPublicContests({type: contestType})
+  const {data: contests, isLoading} = useGetPublicContests({type: contestType})
+  const {data: contestCounts} = useGetPublicContestCounts()
 
   return (
     <Tabs value={contestType} onValueChange={setContestType}>
       <TabsList className="mb-2">
-        <TabsTrigger value={ContestOccurence.PRESENT}>Active</TabsTrigger>
-        <TabsTrigger value={ContestOccurence.FUTURE}>Upcoming</TabsTrigger>
-        <TabsTrigger value={ContestOccurence.PAST}>Completed</TabsTrigger>
+        <TabsTrigger
+          value={
+            ContestOccurence.PRESENT
+          }>{`Active${formatCount(contestCounts?.present)}`}</TabsTrigger>
+        <TabsTrigger
+          value={
+            ContestOccurence.FUTURE
+          }>{`Upcoming${formatCount(contestCounts?.future)}`}</TabsTrigger>
+        <TabsTrigger
+          value={
+            ContestOccurence.PAST
+          }>{`Completed${formatCount(contestCounts?.past)}`}</TabsTrigger>
       </TabsList>
       {isLoading ? (
         <div className="flex flex-col gap-2">
@@ -27,7 +41,7 @@ const Contests = () => {
           <Skeleton className="h-[60px]" />
         </div>
       ) : (
-        <ContestsTable contests={data ?? []} />
+        <ContestsTable contests={contests ?? []} />
       )}
     </Tabs>
   )
