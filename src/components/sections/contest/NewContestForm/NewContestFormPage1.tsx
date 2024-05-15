292d568769323e1a7811a4a3843a294b4967d4b6
Matej Falat <matej.falat@gmail.com>
2024-05-15 14:13:52 +0200

    feat: page 1 of NewContestForm

--- src/components/sections/contest/NewContestForm/NewContestFormPage1.tsx
+++ src/components/sections/contest/NewContestForm/NewContestFormPage1.tsx
@@ -1,4 +1,6 @@
 import {type NewContestFormPageProps} from './NewContestForm'
+import GithubRepoSelect from '../../github/GithubRepoSelect'
+import GithubFileSelect from '../../github/GithubFileSelect'
 
 import {
   FormControl,
@@ -8,22 +10,126 @@ import {
   FormMessage,
 } from '@/components/ui/Form'
 import {Input} from '@/components/ui/Input'
+import MarkdownEditor from '@/components/markdown/MarkdownEditor'
+import {CheckboxGroup} from '@/components/ui/Checkbox'
+import {selectOptions} from '@/lib/utils/common/enums'
 
-const NewContestFormPage1 = ({control}: NewContestFormPageProps) => {
+export const page1fields = [
+  'title',
+  'repository',
+  'filesInScope',
+  'projectCategory',
+  'projectLanguage',
+  'description',
+] as const
+
+const NewContestFormPage1 = ({form}: NewContestFormPageProps) => {
+  const {control, watch, setValue} = form
+
+  const selectedRepo = watch('repository')
+
   return (
-    <FormField
-      control={control}
-      name="title"
-      render={({field}) => (
-        <FormItem>
-          <FormLabel>What is the name of your project?</FormLabel>
-          <FormControl>
-            <Input {...field} />
-          </FormControl>
-          <FormMessage />
-        </FormItem>
+    <div className="flex flex-col gap-5">
+      <FormField
+        control={control}
+        name="title"
+        render={({field}) => (
+          <FormItem>
+            <FormLabel>What is the name of your project?</FormLabel>
+            <FormControl>
+              <Input {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={control}
+        name="repository"
+        render={({field}) => (
+          <FormItem>
+            <FormLabel>Link your Github repository</FormLabel>
+            <FormControl>
+              <GithubRepoSelect
+                selectedRepo={field.value}
+                onSelectRepo={(repo) => {
+                  field.onChange(repo)
+                  setValue('filesInScope', [])
+                }}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      {selectedRepo && (
+        <FormField
+          control={control}
+          name="filesInScope"
+          disabled={!selectedRepo}
+          render={({field}) => (
+            <FormItem>
+              <FormLabel>Scope definition</FormLabel>
+              <FormControl>
+                <GithubFileSelect
+                  selectedRepo={selectedRepo}
+                  selectedFiles={field.value}
+                  onSelectFiles={field.onChange}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
       )}
-    />
+      <FormField
+        control={control}
+        name="projectCategory"
+        render={({field: {ref, ...field}}) => (
+          <FormItem>
+            <FormLabel>Project type</FormLabel>
+            <FormControl>
+              <CheckboxGroup
+                options={selectOptions.projectCategory}
+                className="flex-row flex-wrap gap-6"
+                {...field}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={control}
+        name="projectLanguage"
+        render={({field: {ref, ...field}}) => (
+          <FormItem>
+            <FormLabel>Language</FormLabel>
+            <FormControl>
+              <CheckboxGroup
+                options={selectOptions.projectLanguage}
+                className="flex-row flex-wrap gap-6"
+                {...field}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={control}
+        name="description"
+        render={({field}) => (
+          <FormItem>
+            <FormLabel>Description</FormLabel>
+            <FormControl>
+              <MarkdownEditor {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    </div>
   )
 }
 
