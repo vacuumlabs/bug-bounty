75f8f698521c7d5022e22e02b4499d07c477c884
Matej Falat <matej.falat@gmail.com>
2024-05-15 10:32:02 +0200

    chore: make MarkdownEditor controlled

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -41,12 +41,16 @@ export type ButtonProps = {
   VariantProps<typeof buttonVariants>
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({className, variant, size, asChild = false, ...props}, ref) => {
+  (
+    {className, variant, size, asChild = false, type = 'button', ...props},
+    ref,
+  ) => {
     const Comp = asChild ? Slot : 'button'
     return (
       <Comp
         className={cn(buttonVariants({variant, size, className}))}
         ref={ref}
+        type={type}
         {...props}
       />
     )
