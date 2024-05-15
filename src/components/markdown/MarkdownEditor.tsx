00d453bd62e3a6dcd325ccfaddd90f4094a13dae
Matej Falat <matej.falat@gmail.com>
2024-05-15 10:32:02 +0200

    chore: make MarkdownEditor controlled

--- src/components/markdown/MarkdownEditor.tsx
+++ src/components/markdown/MarkdownEditor.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import {useState} from 'react'
+import {forwardRef, useState} from 'react'
 
 import {Textarea} from '../ui/Textarea'
 import {Button} from '../ui/Button'
@@ -10,32 +10,40 @@ import {cn} from '@/lib/utils/client/tailwind'
 
 type MarkdownEditorProps = {
   className?: string
+  value: string | null
+  onChange: (value: string) => void
+  onBlur?: () => void
+  name?: string
 }
 
-const MarkdownEditor = ({className}: MarkdownEditorProps) => {
-  const [doc, setDoc] = useState('')
-  const [isPreview, setIsPreview] = useState(false)
+const MarkdownEditor = forwardRef<HTMLTextAreaElement, MarkdownEditorProps>(
+  ({className, name, value, onBlur, onChange}, ref) => {
+    const [isPreview, setIsPreview] = useState(false)
 
-  return (
-    <div className={cn('w-full', className)}>
-      <Button
-        className="mb-2"
-        variant="outline"
-        onClick={() => setIsPreview(!isPreview)}>
-        {isPreview ? 'Edit' : 'Preview'}
-      </Button>
-
-      {isPreview ? (
-        <MarkdownPreview className="min-h-[160px]" doc={doc} />
-      ) : (
-        <Textarea
-          className="min-h-[160px]"
-          onChange={(event) => setDoc(event.target.value)}
-          value={doc}
-        />
-      )}
-    </div>
-  )
-}
+    return (
+      <div className={cn('flex flex-col gap-2', className)}>
+        <Button
+          className="self-end"
+          variant="outline"
+          onClick={() => setIsPreview(!isPreview)}>
+          {isPreview ? 'Edit' : 'Preview'}
+        </Button>
+        {isPreview ? (
+          <MarkdownPreview className="min-h-[260px]" doc={value ?? ''} />
+        ) : (
+          <Textarea
+            ref={ref}
+            onBlur={onBlur}
+            className="min-h-[260px]"
+            onChange={(event) => onChange(event.target.value)}
+            value={value ?? ''}
+            name={name}
+          />
+        )}
+      </div>
+    )
+  },
+)
+MarkdownEditor.displayName = 'MarkdownEditor'
 
 export default MarkdownEditor
