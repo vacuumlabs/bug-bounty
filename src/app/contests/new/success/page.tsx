53abb666f25e27fc9a4fe31c2e828c7ed51f3e6a
Matej Falat <matej.falat@gmail.com>
2024-05-17 17:28:28 +0200

    feat: contest added page

---
+++ src/app/contests/new/success/page.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+
+import {Button} from '@/components/ui/Button'
+import {requirePageSession} from '@/server/utils/auth'
+
+const NewContestSuccessPage = async () => {
+  await requirePageSession()
+
+  return (
+    <main className="flex flex-grow flex-col items-center px-6 py-[134px]">
+      <h1 className="text-lg font-bold">
+        Thank you for submitting a new audit!
+      </h1>
+      <p className="mb-9 mt-4">
+        Thank you for submitting your audit! Our team will begin reviewing it
+        promptly.
+      </p>
+      <Button asChild variant="default">
+        <Link href="/contests">Go to dashboard</Link>
+      </Button>
+    </main>
+  )
+}
+
+export default NewContestSuccessPage
