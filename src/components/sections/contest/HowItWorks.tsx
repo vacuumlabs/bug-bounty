1f035f87c6df3c3885acdd8490a11f9ddb01681f
Matej Falat <matej.falat@gmail.com>
2024-05-21 11:23:53 +0200

    feat: add How it works section

---
+++ src/components/sections/contest/HowItWorks.tsx
@@ -0,0 +1,99 @@
+import Link from 'next/link'
+
+const items = [
+  {
+    title: 'Explore',
+    description: (
+      <span>
+        Explore our list of{' '}
+        <Link className="underline" href="/contests">
+          Active Bounties
+        </Link>{' '}
+        and read its details and terms.
+      </span>
+    ),
+  },
+  {
+    title: 'Sign in',
+    description: (
+      <span>
+        Explore our list of{' '}
+        <Link className="underline" href="/contests">
+          Active Bounties
+        </Link>{' '}
+        and read its details and terms.
+      </span>
+    ),
+  },
+  {
+    title: 'Connect',
+    description: (
+      <span>
+        Explore our list of{' '}
+        <Link className="underline" href="/contests">
+          Active Bounties
+        </Link>{' '}
+        and read its details and terms.
+      </span>
+    ),
+  },
+  {
+    title: 'Report',
+    description: (
+      <span>
+        Explore our list of{' '}
+        <Link className="underline" href="/contests">
+          Active Bounties
+        </Link>{' '}
+        and read its details and terms.
+      </span>
+    ),
+  },
+  {
+    title: 'Wait',
+    description: (
+      <span>
+        Explore our list of{' '}
+        <Link className="underline" href="/contests">
+          Active Bounties
+        </Link>{' '}
+        and read its details and terms.
+      </span>
+    ),
+  },
+  {
+    title: 'Win',
+    description: (
+      <span>
+        Explore our list of{' '}
+        <Link className="underline" href="/contests">
+          Active Bounties
+        </Link>{' '}
+        and read its details and terms.
+      </span>
+    ),
+  },
+]
+
+const HowItWorks = () => {
+  return (
+    <div className="flex flex-col gap-11">
+      <h2 className="text-4xl font-bold uppercase">How it works?</h2>
+      <div className="flex flex-wrap gap-11">
+        {items.map((item, index) => (
+          <div
+            key={index}
+            className="hover:bg-white-5 flex flex-grow basis-1/4 flex-col gap-4 border-b border-transparent p-4 hover:border-white">
+            <div className="flex h-10 w-10 items-center justify-center rounded-full border border-white text-2xl font-bold">
+              {index + 1}
+            </div>
+            <span className="text-xl font-bold">{item.title}</span>
+            <p className="text-sm">{item.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default HowItWorks
