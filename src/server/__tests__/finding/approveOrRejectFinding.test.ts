04becc0c1d5a6744fcf7d7400d3dd9e18142e3c6
Ice de Vera <46994386+icedevera@users.noreply.github.com>
2024-05-21 22:39:46 +0800

    [BUBO-47] Tests for finding (#39)

---
+++ src/server/__tests__/finding/approveOrRejectFinding.test.ts
@@ -0,0 +1,217 @@
+import {Mock, beforeEach, describe, expect, it, vi} from 'vitest'
+import {subDays} from 'date-fns'
+import {getServerSession} from 'next-auth'
+import {v4 as uuidv4} from 'uuid'
+
+import {trunacateDb} from '../utils/db'
+
+import {
+  ContestStatus,
+  FindingSeverity,
+  FindingStatus,
+  ProjectCategory,
+  ProjectLanguage,
+  UserRole,
+} from '@/server/db/models'
+import {InsertUser} from '@/server/db/schema/user'
+import {db, schema} from '@/server/db'
+import {InsertContest} from '@/server/db/schema/contest'
+import {InsertFinding} from '@/server/db/schema/finding'
+import {
+  ApproveOrRejectFindingRequest,
+  approveOrRejectFindingAction,
+} from '@/server/actions/finding/approveOrRejectFinding'
+
+const userId = uuidv4()
+const contestId = uuidv4()
+const contestAuthorId = uuidv4()
+
+const contestRequest: InsertContest = {
+  id: contestId,
+  authorId: contestAuthorId,
+  title: 'Example Contest',
+  description: 'This is an example contest.',
+  repoUrl: 'https://github.com/example-contest',
+  rewardsAmount: '1000',
+  customConditions: 'There are four custom conditions.',
+  projectCategory: [ProjectCategory.DEFI, ProjectCategory.INFRASTRUCTURE],
+  projectLanguage: [ProjectLanguage.AIKEN],
+  status: ContestStatus.PENDING,
+  distributedRewardsAmount: '0',
+  startDate: subDays(new Date(), 3),
+  endDate: subDays(new Date(), 1),
+}
+
+const findingToInsert: InsertFinding = {
+  contestId: contestId,
+  authorId: userId,
+  description: 'This is a finding description.',
+  title: 'Finding title',
+  severity: FindingSeverity.HIGH,
+  status: FindingStatus.PENDING,
+  targetFileUrl: 'https://github.com/example-contest/file.js',
+  deduplicatedFindingId: null,
+}
+
+vi.mock('next-auth')
+
+describe('approveOrRejectFinding', () => {
+  beforeEach(async () => {
+    vi.resetAllMocks()
+
+    await trunacateDb()
+
+    const users: InsertUser[] = [
+      {
+        id: userId,
+        email: 'user@example.com',
+      },
+      {
+        id: contestAuthorId,
+        email: 'contestAuthor@example.com',
+      },
+    ]
+
+    await db.insert(schema.users).values(users)
+  })
+
+  it('throws an error if the finding is not PENDING', async () => {
+    ;(getServerSession as Mock).mockResolvedValue({
+      user: {
+        id: userId,
+        provider: 'github',
+        role: UserRole.JUDGE,
+      },
+      expires: new Date(Date.now() + 86_400_000).toISOString(),
+    })
+
+    const contest = await db
+      .insert(schema.contests)
+      .values([{...contestRequest, status: ContestStatus.DRAFT}])
+      .returning()
+
+    if (contest.length === 0 || !contest[0]?.id) {
+      throw new Error('Contest not found.')
+    }
+
+    const insertedFinding = await db
+      .insert(schema.findings)
+      .values({...findingToInsert, status: FindingStatus.DRAFT})
+      .returning()
+
+    const insertedFindingId = insertedFinding[0]?.id
+
+    if (insertedFinding.length === 0 || !insertedFindingId) {
+      throw new Error('Finding not found.')
+    }
+
+    const approveRequest: ApproveOrRejectFindingRequest = {
+      findingId: insertedFindingId,
+      newStatus: FindingStatus.APPROVED,
+    }
+
+    await expect(async () => {
+      await approveOrRejectFindingAction(approveRequest)
+    }).rejects.toThrowError('Only pending findings can be approved/rejected.')
+  })
+
+  it('approves a finding successfully', async () => {
+    ;(getServerSession as Mock).mockResolvedValue({
+      user: {
+        id: userId,
+        provider: 'github',
+        role: UserRole.JUDGE,
+      },
+      expires: new Date(Date.now() + 86_400_000).toISOString(),
+    })
+
+    const insertedContest = await db
+      .insert(schema.contests)
+      .values([contestRequest])
+      .returning()
+
+    if (insertedContest.length === 0 || !insertedContest[0]?.id) {
+      throw new Error('Contest not found.')
+    }
+
+    const insertedFinding = await db
+      .insert(schema.findings)
+      .values([findingToInsert])
+      .returning()
+
+    const insertedFindingId = insertedFinding[0]?.id
+
+    if (insertedFinding.length === 0 || !insertedFindingId) {
+      throw new Error('Finding not found.')
+    }
+
+    const approveRequest: ApproveOrRejectFindingRequest = {
+      findingId: insertedFindingId,
+      newStatus: FindingStatus.APPROVED,
+    }
+
+    const result = await approveOrRejectFindingAction(approveRequest)
+    const approvedFinding = result[0]
+
+    if (!approvedFinding) {
+      throw new Error('Approved contest not found.')
+    }
+
+    const dbData = await db.query.findings.findFirst({
+      where: (findings, {eq}) => eq(findings.id, approvedFinding.id),
+    })
+
+    expect(approvedFinding).toEqual(dbData)
+    expect(dbData?.status).toEqual(FindingStatus.APPROVED)
+  })
+
+  it('rejects a finding successfully', async () => {
+    ;(getServerSession as Mock).mockResolvedValue({
+      user: {
+        id: userId,
+        provider: 'github',
+        role: UserRole.JUDGE,
+      },
+      expires: new Date(Date.now() + 86_400_000).toISOString(),
+    })
+
+    const insertedContest = await db
+      .insert(schema.contests)
+      .values([contestRequest])
+      .returning()
+
+    if (insertedContest.length === 0 || !insertedContest[0]?.id) {
+      throw new Error('Contest not found.')
+    }
+
+    const insertedFinding = await db
+      .insert(schema.findings)
+      .values([findingToInsert])
+      .returning()
+
+    const insertedFindingId = insertedFinding[0]?.id
+
+    if (insertedFinding.length === 0 || !insertedFindingId) {
+      throw new Error('Finding not found.')
+    }
+
+    const approveRequest: ApproveOrRejectFindingRequest = {
+      findingId: insertedFindingId,
+      newStatus: FindingStatus.REJECTED,
+    }
+
+    const result = await approveOrRejectFindingAction(approveRequest)
+    const approvedFinding = result[0]
+
+    if (!approvedFinding) {
+      throw new Error('Rejected contest not found.')
+    }
+
+    const dbData = await db.query.findings.findFirst({
+      where: (findings, {eq}) => eq(findings.id, approvedFinding.id),
+    })
+
+    expect(approvedFinding).toEqual(dbData)
+    expect(dbData?.status).toEqual(FindingStatus.REJECTED)
+  })
+})
---
+++ src/server/__tests__/finding/editFinding.test.ts
@@ -0,0 +1,146 @@
+import {describe, it, expect, vi, Mock, beforeEach} from 'vitest'
+import {addDays} from 'date-fns'
+import {getServerSession} from 'next-auth'
+import {v4 as uuidv4} from 'uuid'
+
+import {trunacateDb} from '../utils/db'
+import {expectAnyDate} from '../utils/expect'
+
+import {
+  ContestStatus,
+  FindingSeverity,
+  FindingStatus,
+  ProjectCategory,
+  UserRole,
+} from '@/server/db/models'
+import {db, schema} from '@/server/db'
+import {InsertUser} from '@/server/db/schema/user'
+import {InsertContest} from '@/server/db/schema/contest'
+import {Finding, InsertFinding} from '@/server/db/schema/finding'
+import {
+  EditFindingRequest,
+  editFindingAction,
+} from '@/server/actions/finding/editFinding'
+
+const userId = uuidv4()
+const contestId = uuidv4()
+const contestAuthorId = uuidv4()
+
+const contestToInsert: InsertContest = {
+  id: contestId,
+  authorId: contestAuthorId,
+  title: 'Example Contest',
+  description: 'This is an example contest.',
+  repoUrl: 'https://github.com/example-contest',
+  rewardsAmount: '1000',
+  customConditions: 'There are four custom conditions.',
+  projectCategory: [ProjectCategory.DEFI, ProjectCategory.INFRASTRUCTURE],
+  status: ContestStatus.PENDING,
+  distributedRewardsAmount: '0',
+  startDate: addDays(new Date(), 1),
+  endDate: addDays(new Date(), 2),
+}
+
+const findingToInsert: InsertFinding = {
+  contestId: contestId,
+  authorId: userId,
+  description: 'This is a finding description.',
+  title: 'Finding title',
+  severity: FindingSeverity.HIGH,
+  status: FindingStatus.DRAFT,
+  targetFileUrl: 'https://github.com/example-contest/file.js',
+  deduplicatedFindingId: null,
+}
+
+vi.mock('next-auth')
+
+describe('editFinding', () => {
+  beforeEach(async () => {
+    vi.resetAllMocks()
+
+    await trunacateDb()
+
+    const users: InsertUser[] = [
+      {
+        id: userId,
+        email: 'user@example.com',
+      },
+      {
+        id: contestAuthorId,
+        email: 'contestAuthor@example.com',
+      },
+    ]
+
+    await db.insert(schema.users).values(users)
+  })
+
+  it('edits a finding successfully', async () => {
+    ;(getServerSession as Mock).mockResolvedValue({
+      user: {
+        id: userId,
+        provider: 'github',
+        role: UserRole.AUDITOR,
+      },
+      expires: new Date(Date.now() + 86_400_000).toISOString(),
+    })
+
+    const insertedContests = await db
+      .insert(schema.contests)
+      .values([contestToInsert])
+      .returning()
+
+    if (insertedContests.length === 0 || !insertedContests[0]) {
+      throw new Error('No contest created.')
+    }
+
+    const insertedFinding = await db
+      .insert(schema.findings)
+      .values({...findingToInsert, status: FindingStatus.DRAFT})
+      .returning()
+
+    const insertedFindingId = insertedFinding[0]?.id
+
+    if (insertedFinding.length === 0 || !insertedFindingId) {
+      throw new Error('Finding not found.')
+    }
+
+    const editFindingRequest: EditFindingRequest = {
+      finding: {
+        id: insertedFindingId,
+        description: 'This is an updated finding description.',
+        title: 'Updated finding title',
+        severity: FindingSeverity.LOW,
+        status: FindingStatus.PENDING,
+        targetFileUrl: 'https://github.com/example-contest/file-2.js',
+      },
+    }
+
+    const result = await editFindingAction(editFindingRequest)
+
+    const dbFinding = await db.query.findings.findFirst({
+      where: (findings, {eq}) => eq(findings.id, insertedFindingId),
+    })
+
+    if (!dbFinding) {
+      throw new Error('Finding not found.')
+    }
+
+    expect(result).toEqual([dbFinding])
+
+    const expectedResult: Finding = {
+      id: insertedFindingId,
+      title: 'Updated finding title',
+      description: 'This is an updated finding description.',
+      severity: FindingSeverity.LOW,
+      status: FindingStatus.PENDING,
+      targetFileUrl: 'https://github.com/example-contest/file-2.js',
+      contestId,
+      authorId: userId,
+      createdAt: expectAnyDate,
+      updatedAt: expectAnyDate,
+      deduplicatedFindingId: null,
+    }
+
+    expect(result).toEqual([expectedResult])
+  })
+})
