327ee385f4b4072a457a5625fceae6ee3cb1a95d
Matej Falat <matej.falat@gmail.com>
2024-05-24 17:08:57 +0200

    chore: typography system update

--- src/components/sections/contest/HowItWorks.tsx
+++ src/components/sections/contest/HowItWorks.tsx
@@ -78,13 +78,13 @@ const items = [
 const HowItWorks = () => {
   return (
     <div className="flex flex-col gap-11">
-      <h2 className="text-displayS uppercase">How it works?</h2>
+      <h2 className="text-headlineM uppercase">How it works?</h2>
       <div className="flex flex-wrap gap-11">
         {items.map((item, index) => (
           <div
             key={index}
             className="flex flex-grow basis-1/4 flex-col gap-4 border-b border-transparent p-4 hover:border-white hover:bg-white/5">
-            <div className="flex h-10 w-10 items-center justify-center rounded-full border border-white text-headlineS font-bold">
+            <div className="flex h-10 w-10 items-center justify-center rounded-full border border-white text-titleM font-bold">
               {index + 1}
             </div>
             <span className="text-buttonL">{item.title}</span>
