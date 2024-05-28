430ddc6adf42cde304b85ac7694c17b8b2184b9a
Matej Falat <matej.falat@gmail.com>
2024-05-28 16:21:28 +0200

    chore: update popover and checkbox styling

--- src/components/ui/Checkbox/Checkbox.tsx
+++ src/components/ui/Checkbox/Checkbox.tsx
@@ -1,8 +1,8 @@
 'use client'
 
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox'
-import {CheckIcon} from '@radix-ui/react-icons'
 import {ComponentPropsWithoutRef, ElementRef, forwardRef} from 'react'
+import {Check} from 'lucide-react'
 
 import {cn} from '@/lib/utils/client/tailwind'
 
@@ -13,13 +13,13 @@ const Checkbox = forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      'border-primary focus-visible:ring-ring data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground peer h-4 w-4 shrink-0 rounded-sm border shadow focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50',
+      'border-primary focus-visible:ring-ring data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground peer h-6 w-6 shrink-0 border shadow focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50',
       className,
     )}
     {...props}>
     <CheckboxPrimitive.Indicator
       className={cn('flex items-center justify-center text-current')}>
-      <CheckIcon className="h-4 w-4" />
+      <Check className="h-4 w-4" />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
 ))
