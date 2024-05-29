70252174531cdba8932dbe9d36ce4dcd1c515a47
Matej Falat <matej.falat@gmail.com>
2024-05-29 15:03:29 +0200

    chore: force github sign in before starting the new contest form

--- src/app/(projects)/my-projects/new/page.tsx
+++ src/app/(projects)/my-projects/new/page.tsx
@@ -1,9 +1,10 @@
 import {NewContestForm} from '@/components/sections/contest/NewContestForm'
+import GithubSignInButton from '@/components/sections/contest/NewContestForm/GithubSignInButton'
 import FormBackButton from '@/components/ui/FormBackButton'
 import {requirePageSession} from '@/server/utils/auth'
 
 const NewContestPage = async () => {
-  await requirePageSession()
+  const session = await requirePageSession()
 
   return (
     <main className="flex flex-grow flex-col items-center px-24 pb-20 pt-6">
@@ -12,7 +13,11 @@ const NewContestPage = async () => {
         <h1 className="mb-12 self-center text-headlineS uppercase">
           Set up a new audit
         </h1>
-        <NewContestForm />
+        {session.user.provider === 'github' ? (
+          <NewContestForm />
+        ) : (
+          <GithubSignInButton className="mt-10 self-center" />
+        )}
       </div>
     </main>
   )
