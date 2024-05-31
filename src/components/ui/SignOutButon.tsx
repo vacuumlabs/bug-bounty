23ae4077eaa6e5883213b6bc61f1e69c29687d11
Matej Falat <matej.falat@gmail.com>
2024-05-31 09:03:49 +0200

    chore: set default sign out callback url to homepage

--- src/components/ui/SignOutButon.tsx
+++ src/components/ui/SignOutButon.tsx
@@ -4,11 +4,16 @@ import {signOut} from 'next-auth/react'
 
 import {Button, ButtonProps} from './Button'
 
+import {PATHS} from '@/lib/utils/common/paths'
+
 type SignOutButtonProps = Omit<ButtonProps, 'onClick'> & {
   callbackUrl?: string
 }
 
-const SignOutButton = ({callbackUrl, ...props}: SignOutButtonProps) => (
+const SignOutButton = ({
+  callbackUrl = PATHS.home,
+  ...props
+}: SignOutButtonProps) => (
   <Button {...props} onClick={() => signOut({callbackUrl})} />
 )
 
