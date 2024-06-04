c89b52772db4a6fe15d77631dca7a96fd8e60ab6
Matej Falat <matej.falat@gmail.com>
2024-06-04 15:54:34 +0200

    chore: add signout popup

--- src/components/ui/Command/CommandDialog.tsx
+++ src/components/ui/Command/CommandDialog.tsx
@@ -4,7 +4,7 @@ import {type DialogProps} from '@radix-ui/react-dialog'
 
 import Command from './Command'
 
-import {DialogRoot, DialogContent} from '@/components/ui/Dialog'
+import {DialogRoot, DialogContent, DialogClose} from '@/components/ui/Dialog'
 
 type CommandDialogProps = DialogProps
 
@@ -15,6 +15,7 @@ const CommandDialog = ({children, ...props}: CommandDialogProps) => {
         <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-slate-500 [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
           {children}
         </Command>
+        <DialogClose />
       </DialogContent>
     </DialogRoot>
   )
--- src/components/ui/SignOutButon.tsx
+++ src/components/ui/SignOutButon.tsx
@@ -1,10 +1,14 @@
 'use client'
 
 import {signOut} from 'next-auth/react'
+import {useEffect, useState} from 'react'
 
 import {Button, ButtonProps} from './Button'
+import {DialogContent, DialogPortal, DialogRoot, DialogTrigger} from './Dialog'
 
 import {PATHS} from '@/lib/utils/common/paths'
+
+const POPUP_CLOSE_DELAY = 3000
 
 type SignOutButtonProps = Omit<ButtonProps, 'onClick'> & {
   callbackUrl?: string
@@ -13,8 +17,27 @@ type SignOutButtonProps = Omit<ButtonProps, 'onClick'> & {
 const SignOutButton = ({
   callbackUrl = PATHS.home,
   ...props
-}: SignOutButtonProps) => (
-  <Button {...props} onClick={() => signOut({callbackUrl})} />
-)
+}: SignOutButtonProps) => {
+  const [isPopupOpen, setIsPopupOpen] = useState(false)
+
+  useEffect(() => {
+    if (isPopupOpen) {
+      setTimeout(() => setIsPopupOpen(false), POPUP_CLOSE_DELAY)
+    }
+  }, [isPopupOpen])
+
+  return (
+    <DialogRoot open={isPopupOpen} onOpenChange={setIsPopupOpen}>
+      <DialogTrigger asChild>
+        <Button {...props} onClick={() => signOut({callbackUrl})} />
+      </DialogTrigger>
+      <DialogPortal>
+        <DialogContent className="w-[500px] border-0 bg-grey-90 p-12 text-center text-titleL uppercase">
+          You have successfully logged out
+        </DialogContent>
+      </DialogPortal>
+    </DialogRoot>
+  )
+}
 
 export default SignOutButton
