f67686cfda8565ab3f23726eb16b9ea9879da89a
Ice de Vera <iestyndevera@gmail.com>
2024-06-10 13:33:26 +0800

    don't add callbackUrl if undefined

--- src/components/sections/auth/OAuthButtons.tsx
+++ src/components/sections/auth/OAuthButtons.tsx
@@ -13,7 +13,7 @@ type OAuthButtonsProps = {
 }
 
 const OAuthButtons = ({callbackUrl}: OAuthButtonsProps) => {
-  const redirectUrl = `${PATHS.selectRole}?callbackUrl=${callbackUrl ?? PATHS.home}`
+  const redirectUrl = `${PATHS.selectRole}${callbackUrl ? `?callbackUrl=${callbackUrl}` : ''}`
 
   return (
     <div className="flex flex-col gap-6">
