67a66196624811a324c8095f9404a15a13837529
Matej Falat <matej.falat@gmail.com>
2024-06-13 18:15:45 +0200

    feat: New finding form page 1

--- src/app/(projects)/finding/new/page.tsx
+++ src/app/(projects)/finding/new/page.tsx
@@ -1,19 +1,30 @@
 import HydrationBoundary from '@/components/helpers/HydrationBoundary'
-import NewFindingForm from '@/components/sections/finding/NewFindingForm'
+import NewFindingForm from '@/components/sections/finding/NewFindingForm/NewFindingForm'
+import FormBackButton from '@/components/ui/FormBackButton'
 import {prefetchGetPublicContests} from '@/lib/queries/contest/getPublicContests'
+import {prefetchGetUser} from '@/lib/queries/user/getUser'
 import {ContestOccurence} from '@/server/db/models'
 import {requirePageSession} from '@/server/utils/auth'
 
 const NewFindingPage = async () => {
-  await requirePageSession()
-  await prefetchGetPublicContests({type: ContestOccurence.PRESENT})
+  const session = await requirePageSession()
+
+  await Promise.all([
+    prefetchGetUser(session.user.id),
+    prefetchGetPublicContests({type: ContestOccurence.PRESENT}),
+  ])
 
   return (
-    <main className="flex max-w-screen-lg flex-col gap-8 px-10 pb-20 pt-10">
-      <h1 className="text-3xl font-semibold">New Finding</h1>
-      <HydrationBoundary>
-        <NewFindingForm />
-      </HydrationBoundary>
+    <main className="flex flex-grow flex-col items-center px-24 pb-20 pt-6">
+      <FormBackButton numberOfPages={3} className="self-start" />
+      <div className="flex w-full max-w-3xl flex-col">
+        <h1 className="mb-12 self-center text-headlineS uppercase">
+          New report submission
+        </h1>
+        <HydrationBoundary>
+          <NewFindingForm />
+        </HydrationBoundary>
+      </div>
     </main>
   )
 }
---
+++ src/components/sections/finding/NewFindingForm/NewFindingForm.tsx
@@ -0,0 +1,120 @@
+'use client'
+
+import {useRouter} from 'next/navigation'
+import {UseFormReturn, useForm} from 'react-hook-form'
+import {z} from 'zod'
+import {zodResolver} from '@hookform/resolvers/zod'
+import {ArrowRight} from 'lucide-react'
+
+import NewFindingFormPage1, {page1fields} from './NewFindingFormPage1'
+
+import {Form} from '@/components/ui/Form'
+import {addFindingSchema} from '@/server/utils/validations/schemas'
+import {useAddFinding} from '@/lib/queries/finding/addFinding'
+import {Button} from '@/components/ui/Button'
+import {FindingStatus} from '@/server/db/models'
+import {toast} from '@/components/ui/Toast'
+import {Tabs, TabsContent} from '@/components/ui/Tabs'
+import FormPagination from '@/components/ui/FormPagination'
+import {useSearchParamsState} from '@/lib/hooks/useSearchParamsState'
+import {useFormPageSearchParams} from '@/lib/hooks/useFormPageSearchParams'
+import {Nullable} from '@/lib/types/general'
+import {PATHS} from '@/lib/utils/common/paths'
+import {useGetUser} from '@/lib/queries/user/getUser'
+
+const formPageTitles = ['Basic information', 'Report description', 'Review']
+
+const formSchema = addFindingSchema
+  .omit({
+    status: true,
+  })
+  .extend({
+    attachments: z.instanceof(File).array().optional(),
+  })
+
+type FormValues = z.infer<typeof formSchema>
+type InputFormValues = Nullable<FormValues>
+
+export type NewFindingFormPageProps = {
+  form: UseFormReturn<InputFormValues, unknown, FormValues>
+}
+
+const NewFindingForm = () => {
+  const router = useRouter()
+  const [page, setPage] = useFormPageSearchParams(3)
+  const [contestId] = useSearchParamsState('contestId')
+
+  const {data: user} = useGetUser()
+
+  const form = useForm<InputFormValues, unknown, FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      contestId,
+      rewardWalletAddress: user?.walletAddress ?? null,
+      title: '',
+      description: '',
+      proofOfConcept: '',
+      affectedFiles: [],
+      attachments: [],
+    },
+  })
+
+  const {mutate, isPending} = useAddFinding()
+
+  const {handleSubmit, trigger} = form
+
+  const addFinding = ({attachments, ...values}: FormValues) => {
+    mutate(
+      {finding: {...values, status: FindingStatus.PENDING}, attachments},
+      {
+        onSuccess: () => router.push(PATHS.newFindingSuccess),
+      },
+    )
+  }
+
+  const onSubmit = handleSubmit(addFinding, () =>
+    toast({title: 'Submission failed.', description: 'Invalid input values.'}),
+  )
+
+  const onContinue = async () => {
+    const isValid = await trigger(page === 1 ? page1fields : [])
+
+    if (isValid) {
+      setPage(page + 1)
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <Tabs
+        value={page.toString()}
+        onValueChange={(value) => setPage(Number(value))}
+        className="flex flex-col">
+        <FormPagination
+          currentIndex={page - 1}
+          pages={formPageTitles}
+          className="mb-24"
+        />
+        <TabsContent forceMount hidden={page !== 1} value="1">
+          <NewFindingFormPage1 form={form} />
+        </TabsContent>
+        <TabsContent forceMount hidden={page !== 2} value="2"></TabsContent>
+        <TabsContent value="3"></TabsContent>
+      </Tabs>
+      <div className="flex justify-end">
+        {page === 3 ? (
+          <Button type="submit" disabled={isPending} onClick={onSubmit}>
+            {isPending ? 'Submitting...' : 'Submit for review'}
+          </Button>
+        ) : (
+          <Button onClick={onContinue} className="gap-2">
+            Continue
+            <ArrowRight />
+          </Button>
+        )}
+      </div>
+    </Form>
+  )
+}
+
+export default NewFindingForm
