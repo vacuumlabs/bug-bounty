3602e4d2af736f43c2ec2c65c28e60d13f68baa5
Ice de Vera <iestyndevera@gmail.com>
2024-06-14 15:13:31 +0800

    use badge component and use translateEnum

--- src/components/sections/submissions/MySubmissionSeverityBadge.tsx
+++ src/components/sections/submissions/MySubmissionSeverityBadge.tsx
@@ -1,4 +1,6 @@
+import {Badge} from '@/components/ui/Badge'
 import {cn} from '@/lib/utils/client/tailwind'
+import {translateEnum} from '@/lib/utils/common/enums'
 import {FindingSeverity} from '@/server/db/models'
 
 const severityToColor = {
@@ -9,15 +11,17 @@ const severityToColor = {
   [FindingSeverity.CRITICAL]: 'bg-red',
 }
 
-const MySubmissionSeverityBadge = ({severity}: {severity: FindingSeverity}) => {
+const MySubmissionSeverityBadge = ({
+  severity,
+  className,
+}: {
+  severity: FindingSeverity
+  className?: string
+}) => {
   return (
-    <span
-      className={cn(
-        'h-6 w-12 rounded-[40px] px-3 py-1 text-buttonS capitalize text-black',
-        severityToColor[severity],
-      )}>
-      {severity.valueOf()}
-    </span>
+    <Badge className={cn(severityToColor[severity], className)}>
+      {translateEnum.findingSeverity(severity)}
+    </Badge>
   )
 }
 
