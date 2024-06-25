e0023b4ae7f76a51f02a9159add398c1f5f8fc44
Ice de Vera <iestyndevera@gmail.com>
2024-06-25 19:09:35 +0800

    use default severity weights instead of empty value for ContestSeverityWeightsDisplay

--- src/components/ui/ContestSeverityWeightsDisplay.tsx
+++ src/components/ui/ContestSeverityWeightsDisplay.tsx
@@ -1,3 +1,5 @@
+import {defaultSeverityWeights} from '@/lib/utils/common/contest'
+
 type ContestSeverityWeightsProps = {
   info: number | undefined
   low: number | undefined
@@ -17,27 +19,37 @@ const ContestSeverityWeightsDisplay = ({
     <div className="flex gap-6">
       <div className="inline-block">
         <span className="text-bodyM capitalize text-grey-30">info: </span>
-        <span className="text-bodyM text-white">{info ?? '-'}</span>
+        <span className="text-bodyM text-white">
+          {info ?? defaultSeverityWeights.info}
+        </span>
       </div>
       <div className="h-6 w-[1px] bg-white" />
       <div className="inline-block">
         <span className="text-bodyM capitalize text-grey-30">low: </span>
-        <span className="text-bodyM text-white">{low ?? '-'}</span>
+        <span className="text-bodyM text-white">
+          {low ?? defaultSeverityWeights.low}
+        </span>
       </div>
       <div className="h-6 w-[1px] bg-white" />
       <div className="inline-block">
         <span className="text-bodyM capitalize text-grey-30">medium: </span>
-        <span className="text-bodyM text-white">{medium ?? '-'}</span>
+        <span className="text-bodyM text-white">
+          {medium ?? defaultSeverityWeights.medium}
+        </span>
       </div>
       <div className="h-6 w-[1px] bg-white" />
       <div className="inline-block">
         <span className="text-bodyM capitalize text-grey-30">high: </span>
-        <span className="text-bodyM text-white">{high ?? '-'}</span>
+        <span className="text-bodyM text-white">
+          {high ?? defaultSeverityWeights.high}
+        </span>
       </div>
       <div className="h-6 w-[1px] bg-white" />
       <div className="inline-block">
         <span className="text-bodyM capitalize text-grey-30">critical: </span>
-        <span className="text-bodyM text-white">{critical ?? '-'}</span>
+        <span className="text-bodyM text-white">
+          {critical ?? defaultSeverityWeights.critical}
+        </span>
       </div>
     </div>
   )
