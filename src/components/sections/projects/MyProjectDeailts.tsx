aaf5d72ed72e013d3c3239f62dc9c038486618c8
Ice de Vera <iestyndevera@gmail.com>
2024-06-25 22:39:08 +0800

    add project detail path and page

---
+++ src/components/sections/projects/MyProjectDeailts.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+type MyProjectDetailsProps = {
+  contestId: string
+}
+
+const MyProjectDetails = ({contestId}: MyProjectDetailsProps) => {
+  return <div></div>
+}
+
+export default MyProjectDetails
