e976e515d5eca08a1e060ecb55d08464dab9826e
Ice de Vera <iestyndevera@gmail.com>
2024-07-16 18:53:18 +0800

    judge approve or reject contest and contest detail page

--- src/lib/queries/keys.ts
+++ src/lib/queries/keys.ts
@@ -68,8 +68,4 @@ export const queryKeys = createQueryKeyStore({
     repoBranches: (params: GetRepoBranchesParams | undefined) => [params],
     repoFiles: (params: GetRepoFilesParams | undefined) => [params],
   },
-  judges: {
-    contestCounts: null,
-    contests: (params: GetJudgeContestsParams) => [params],
-  },
 })
